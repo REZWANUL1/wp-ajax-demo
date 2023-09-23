fe9707b9abed5956f8c41f0f81f5a0aa637ab380
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-23 22:41:24 +0600

    adding plugin starter header

---
+++ wp-ajax-demo.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name:       Wp Ajax Demo
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rezwanul Haque
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ */
+if (!defined('ABSPATH')) {
+exit;
+}
+
